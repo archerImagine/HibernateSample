2b5508c2f67e46bde45b7134d9604113aaa4981f
Animesh Kumar Bhadra <animesh.bhadra@gmail.com>
2016-04-20 14:01:07 +0530

    first commit

---
+++ src/main/java/com/cisco/cmad/App.java
@@ -0,0 +1,13 @@
+package com.cisco.cmad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
