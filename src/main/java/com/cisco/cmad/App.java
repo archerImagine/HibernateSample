320fb620c68c5f21f0d62c656bed30647931fff0
Animesh Kumar Bhadra <animesh.bhadra@gmail.com>
2016-04-20 16:07:05 +0530

    Connection to database done.

--- src/main/java/com/cisco/cmad/App.java
+++ src/main/java/com/cisco/cmad/App.java
@@ -1,5 +1,7 @@
 package com.cisco.cmad;
 
+import org.hibernate.Session;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Session session =  HibernateUtilities.getSessionFactory().openSession();
+        session.close();
     }
 }
